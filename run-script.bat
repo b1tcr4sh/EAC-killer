4fb02b4855495c39b42773ec16946c1d08eb76f9
Ethan Ford <ethan.ford06@gmail.com>
2022-08-05 15:19:08 -0500

    Added README

--- run-script.bat
+++ run-script.bat
@@ -1,6 +1,5 @@
 :: This script must be run with elevated priviledges (such as a powershell instance with administrator) to kill the EAC process.
-
-cd c:\"Program Files (x86)"\Steam\steamapps\common\VRChat
+pip install psutil
 launch.exe
 
 python kill-eac.py
